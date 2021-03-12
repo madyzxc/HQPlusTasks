b4ac96d86ce9a35a649cd52c6ece4796a9a9782c
Mohamed Ibrahim <madyzxc@hotmail.comecho #README  README.md>
2021-03-12 11:00:22 +0400

    Add project files.

---
+++ WebExtraction/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebExtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
