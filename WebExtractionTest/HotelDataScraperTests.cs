247602945d9c9209ac73348792feca6998a41416
Mohamed <madyzxc@hotmail.com>
2021-03-15 22:40:20 +0400

    adding more test cases

--- WebExtractionTest/HotelDataScraperTests.cs
+++ WebExtractionTest/HotelDataScraperTests.cs
@@ -8,26 +8,41 @@ namespace WebExtractionTest
     public class HotelDataScraperTests
     {
         private IHotelDataScraper _scraper;
+        private Hotel _actualHotel;
         private const string Url = "https://www.booking.com/hotel/de/kempinskibristolberlin.en-gb.html?dcid=4;label=gen173nr-15CAsoO0IWa2VtcGluc2tpYnJpc3RvbGJlcmxpbkguYgVub3JlZmg7iAEBmAEuuAEEyAEE2AED6AEB;type=total;dist=0;auth_success=1;sb_price_type=total;sid=76794d5ed9a9673c09a746b2d3e1a5bd";
 
         [SetUp]
         public void SetUp()
         {
             _scraper = new HotelDataScraper(Url);
+            _actualHotel = _scraper.ScrapeData();
         }
         
         [Test]
         public void ScrapeData_WhenCalled_ReturnsCorrectHotelTitle()
         {
-            var actualHotel = _scraper.ScrapeData();
-            Assert.AreEqual("Hotel Bristol Berlin",actualHotel.Name);
+            Assert.AreEqual("Hotel Bristol Berlin",_actualHotel.Name);
         }
+        
         [Test]
         public void ScrapeData_WhenCalled_ReturnsCorrectHotelAddress()
         {
-            var actualHotel = _scraper.ScrapeData();
-            Assert.AreEqual("Kurfürstendamm 27, Charlottenburg-Wilmersdorf, 10719 Berlin, Germany",actualHotel.Address);
+            Assert.AreEqual("Kurfürstendamm 27, Charlottenburg-Wilmersdorf, 10719 Berlin, Germany",_actualHotel.Address);
         }
+        
+        [Test]
+        public void ScrapeData_WhenCalled_ReturnsCorrectReviewPoints()
+        {
+            Assert.AreEqual(8.3f,_actualHotel.ReviewPoints);
+        }
+        
+        [Test]
+        public void ScrapeData_WhenCalled_ReturnsCorrectNumberOfReviews()
+        {
+            Assert.AreEqual(3558,_actualHotel.NumberOfReviews);
+        }
+        
+        
         
     }
 }
