0b20f93d8b4fe9ac9d8480b1a9b66ae128e6a1b7
Mohamed <madyzxc@hotmail.com>
2021-03-18 02:43:55 +0400

    adding file to be included during build

--- WebExtraction/Program.cs
+++ WebExtraction/Program.cs
@@ -8,11 +8,11 @@ namespace WebExtraction
         {
             const string url = "https://www.booking.com/hotel/de/kempinskibristolberlin.en-gb.html?dcid=4;label=gen173nr-15CAsoO0IWa2VtcGluc2tpYnJpc3RvbGJlcmxpbkguYgVub3JlZmg7iAEBmAEuuAEEyAEE2AED6AEB;type=total;dist=0;auth_success=1;sb_price_type=total;sid=76794d5ed9a9673c09a746b2d3e1a5bd";
             IHotelDataScraper scraper = new HotelDataScraper(url);
-            var hotelBristolBerlin = scraper.ScrapeHotelData();
+            var hotel = scraper.ScrapeHotelData();
 
             const string fileName = "hotel.json";
             IHotelDataFileWriter fileWriter = new HotelDataFileWriter();
-            fileWriter.WriteHotelDataToFile(hotelBristolBerlin,fileName);
+            fileWriter.WriteHotelDataToFile(hotel,fileName);
             
             Console.ReadLine();
         }
